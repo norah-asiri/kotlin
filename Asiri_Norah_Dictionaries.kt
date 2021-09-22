4d5506a5c7a54daec99f7934b75112fffea34508
norah-asiri <58185376+norah-asiri@users.noreply.github.com>
2021-09-22 10:55:40 +0300

    Add files via upload

---
+++ Asiri_Norah_Dictionaries.kt
@@ -0,0 +1,8 @@
+
+fun main() {
+    val WeekDays = mapOf(1 to "Saturday", 2 to "Sunday", 3 to "Monday"
+    ,4 to "Tuesday", 5 to "Wednesday", 6 to "Thursday", 7 to "Friday")
+
+    for (i in 1..7)
+        println(WeekDays[i])
+}
